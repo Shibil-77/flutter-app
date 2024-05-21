67a33ffefa40151ee45d3893b384a7521798642a
shibil <shibi2772@gmail.com>
2024-05-22 02:26:06 +0530

    row and column'

--- lib/widgets/container.dart
+++ lib/widgets/container.dart
@@ -6,53 +6,52 @@ class MyWidget extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text(
-          'My App',
-          style: TextStyle(color: Colors.white),
-        ),
-        centerTitle: true,
-        backgroundColor: Colors.green[900],
-        leading: const Icon(Icons.person, color: Colors.white),
-        actions: [
-          IconButton(
-              onPressed: () {},
-              icon: const Icon(Icons.wallet, color: Colors.white)),
-          IconButton(
-              onPressed: () {},
-              icon: const Icon(
-                Icons.add,
-                color: Colors.white,
-              ))
-        ],
-        shape: const RoundedRectangleBorder(
-            borderRadius: BorderRadius.vertical(
-                // top: Radius.circular(40),
-                bottom: Radius.circular(10))),
-        elevation: 60,
-      ),
-      body: Center(
-          child: Container(
-        width: double.infinity,
-        height: 300,
-        // color: Colors.deepPurpleAccent,
-        // padding:const EdgeInsets.all(20),
-        alignment: Alignment.center,
-        decoration: BoxDecoration(
-            color: Colors.purple,
-            borderRadius: BorderRadius.circular(20),
-            border: Border.all(
-              color: Colors.black,
-              width: 2,
-            )),
-        child: const Text(
-          'Hello',
-          style: TextStyle(
-            color: Colors.white,
-            fontSize: 25,
+        appBar: AppBar(
+          title: const Text(
+            'My App',
+            style: TextStyle(color: Colors.white),
           ),
+          centerTitle: true,
+          backgroundColor: Colors.green[900],
+          leading: const Icon(Icons.person, color: Colors.white),
+          actions: [
+            IconButton(
+                onPressed: () {},
+                icon: const Icon(Icons.wallet, color: Colors.white)),
+            IconButton(
+                onPressed: () {},
+                icon: const Icon(
+                  Icons.add,
+                  color: Colors.white,
+                ))
+          ],
+          shape: const RoundedRectangleBorder(
+              borderRadius: BorderRadius.vertical(
+                  // top: Radius.circular(40),
+                  bottom: Radius.circular(10))),
+          elevation: 60,
         ),
-      )),
-    );
+        body: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          
+          children: [
+            Container(
+              width: 100,
+              height: 100,
+              color: Colors.purple,
+              
+            ),
+            Container(
+              width: 100,
+              height: 100,
+              color: Colors.red,
+            ),
+            Container(
+              width: 100,
+              height: 100,
+              color: Colors.yellow,
+            ),
+          ],
+        ));
   }
 }
